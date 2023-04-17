f91b541cff353eee0ba2faedb2dd39773c6137bc
jonier93 <jonietrporras@gmail.com>
2023-04-17 18:51:55 -0500

    vFM

--- view.py
+++ view.py
@@ -1,3 +1,13 @@
 class View:
-    num1 = input()
-    num2 = input()
+    def __init__(self):
+        print("Enter the option: \n" 
+                "1. Add \n"
+                "2. Substract \n" 
+                "3. Multiply \n"
+                "4. Divide \n")
+        self.option = input()
+        print("Enter the number 1: ")
+        self.num1 = input()
+        print("Enter the number 2: ")
+        self.num2 = input()
+    
